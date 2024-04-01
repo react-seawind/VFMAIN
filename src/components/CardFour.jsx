dae6f17a0e3a48820d972162dd9ab3f5b1be9c92
react-seawind <rajan@seawind.in>
2024-04-01 09:57:15 +0530

    db icon change

--- src/components/CardFour.jsx
+++ src/components/CardFour.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BiCategory } from 'react-icons/bi';
+import { BiBook, BiCategory } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 
 const CardFour = ({ ChapterData }) => {
@@ -7,7 +7,7 @@ const CardFour = ({ ChapterData }) => {
     <Link to={'/school/listing'}>
       <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
-          <BiCategory className="fill-primary dark:fill-white text-2xl" />
+          <BiBook className="fill-primary dark:fill-white text-2xl" />
         </div>
 
         <div className="mt-4 flex items-end justify-between">
